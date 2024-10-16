92cc1cbe00eb64fbd74d081bf2037a92f6dc94b0
FacuGerez <facundogerezuba@gmail.com>
2024-10-16 15:31:46 -0300

    initial structure of repository

---
+++ common/socket_error.h
@@ -0,0 +1,14 @@
+#ifndef SOCKETERROR_H
+#define SOCKETERROR_H
+
+#include <stdexcept>
+#include <string>
+
+class SocketClosedError: public std::runtime_error {
+public:
+    // Constructor que recibe que es lo que se intentaba hacer cuando se cerro el socket
+    explicit SocketClosedError(const char type_operation[]):
+            std::runtime_error("Socket trying to " + std::string(type_operation) +
+                               "but this was close") {}
+};
+#endif
