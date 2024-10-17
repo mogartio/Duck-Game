9278be0c786704b908afeeca8c8e984413d0838b
FacuGerez <facundogerezuba@gmail.com>
2024-10-17 20:29:45 -0300

    first description about what see in a lobby

---
+++ common/descripcion-lobby.h
@@ -0,0 +1,19 @@
+#ifndef DESCRIPCIONLOBBY_H
+#define DESCRIPCIONLOBBY_H
+#include <cstdint>
+#include <string>
+
+class DescripcionLobby {
+public:
+    u_int8_t idLobby;
+    u_int8_t cantidadJugadores;
+    std::string nombreLobby;
+
+    // ------------------ Desabilitamos -----------------------
+    /*
+     * Destructor del mensaje del lobby
+     */
+    ~DescripcionLobby()=default;
+
+};
+#endif
