d501d32aa146e3c5acd9b7572199b77173483ec3
cpiccin <cpiccin@fi.uba.ar>
2024-10-18 23:44:33 -0300

    detalles

--- server/acceptor.cpp
+++ server/acceptor.cpp
@@ -1,4 +1,5 @@
 #include "acceptor.h"
+#include <sys/socket.h>
 
 Acceptor::Acceptor(Socket srv, ClientsMonitor& clients, Queue<std::string>& recv_queue, SendQueuesMonitor& send_queues) 
     : srv(std::move(srv)), clients(clients), recv_queue(recv_queue), send_queues(send_queues) {}
@@ -7,12 +8,12 @@ void Acceptor::run() {
     try {
         int id = 0;
         while (_keep_running) {
-            Socket new_client = srv.accept();
+            Socket new_skt = srv.accept();
 
             Queue<std::string>* new_send_queue = new Queue<std::string>(Q_MAX_SIZE);
             send_queues.add(new_send_queue);
 
-            Client* client = new Client(std::move(new_client), *new_send_queue, recv_queue, id++);
+            Client* client = new Client(std::move(new_skt), *new_send_queue, recv_queue, id++);
             clients.add(client);
 
             reap_dead_clients();
@@ -27,7 +28,7 @@ void Acceptor::reap_dead_clients() {
 }
 
 void Acceptor::stop() {
-    srv.shutdown(2);
+    srv.shutdown(SHUT_RDWR);
     _keep_running = false;
     recv_queue.close();
 }
