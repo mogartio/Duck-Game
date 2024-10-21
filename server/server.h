baab7ae4ef5a99384d01553b568f0dc33a480429
cpiccin <cpiccin@fi.uba.ar>
2024-10-20 22:56:45 -0300

    thread que lee entrada standar

--- server/server.h
+++ server/server.h
@@ -1,23 +1,21 @@
 #ifndef SERVER_H
 #define SERVER_H
 
-#include "../common/queue.h"
 #include "../common/socket.h"
 #include "../common/thread.h"
-#include "./../common/mensaje-lobby.h"
-
-#include "acceptor.h"
+#include "../common/queue.h"
 #include "client.h"
 #include "clients_monitor.h"
 #include "send_queues_monitor.h"
+#include "acceptor.h"
+#include "read_input.h"
 
 class Server {
 private:
     Socket srv;
     ClientsMonitor clients;
-    Queue<MensajeLobby> recv_queue;  // Pongo std::string pero va a cambiar segun el protocolo
-    SendQueuesMonitor<MensajeLobby> send_queues;
-
+    Queue<std::string> recv_queue; // Pongo std::string pero va a cambiar segun el protocolo
+    SendQueuesMonitor<std::string> send_queues;
 public:
     explicit Server(const char* port);
     void run();
