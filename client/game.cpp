727c07b5fa6fdb622b1ad0d946b557c8599155f1
LuCalvert <lulicalvert@gmail.com>
2024-10-21 09:11:18 -0300

    comento switch en game y lobby para que no de error para poder mergear con lobby-manager

--- client/game.cpp
+++ client/game.cpp
@@ -13,40 +13,40 @@ void Game::play() {
 
         //pseudocodigo que explica como funciona lobby
         auto mensaje = queueFrontSender.pop();
-        switch(mensaje[0]) {
-        case PICKUP_EVENT:
-            KeyboardAction msj(1);
-            queueSender.try_push(&msj);
-            break;
+        // switch(mensaje[0]) {
+        // case PICKUP_EVENT:
+        //     KeyboardAction msj(1);
+        //     queueSender.try_push(&msj);
+        //     break;
 
-        case MOVE_LEFT_EVENT:
-            KeyboardAction msj(2);
-            queueSender.push(&msj);
-            break;
+        // case MOVE_LEFT_EVENT:
+        //     KeyboardAction msj(2);
+        //     queueSender.push(&msj);
+        //     break;
         
-        case MOVE_RIGHT_EVENT:
-            KeyboardAction msj(3);
-            queueSender.push(&msj);
-            break;
+        // case MOVE_RIGHT_EVENT:
+        //     KeyboardAction msj(3);
+        //     queueSender.push(&msj);
+        //     break;
 
-        case JUMP_EVENT:
-            KeyboardAction msj(4, mensaje[1]);
-            queueSender.push(&msj);
-            break;
+        // case JUMP_EVENT:
+        //     KeyboardAction msj(4, mensaje[1]);
+        //     queueSender.push(&msj);
+        //     break;
 
-        case PLAY_DEAD_EVENT:
-            KeyboardAction msj(5);
-            queueSender.push(&msj);
-            break;
+        // case PLAY_DEAD_EVENT:
+        //     KeyboardAction msj(5);
+        //     queueSender.push(&msj);
+        //     break;
 
-        case SHOOT_EVENT:
-            KeyboardAction msj(6);
-            queueSender.push(&msj);
-            break;
+        // case SHOOT_EVENT:
+        //     KeyboardAction msj(6);
+        //     queueSender.push(&msj);
+        //     break;
 
-        default:
-            break;
-        }
+        // default:
+        //     break;
+        // }
     }
 
 
--- client/lobby.cpp
+++ client/lobby.cpp
@@ -8,47 +8,47 @@ void Lobby::run() {
 
         //pseudocodigo que explica como funciona lobby
         auto mensaje = queueFrontSender.pop();
-        switch(mensaje[0]) {
-        case PLAY_EVENT:
-            play = true;
-            break;
+        // switch(mensaje[0]) {
+        // case PLAY_EVENT:
+        //     play = true;
+        //     break;
 
-        case VIEW_LOBBIES_EVENT:
-            LobbyAction msj(1, NULL, NULL);
-            queueSender.push(&msj);
-            auto lobbies = queueReciver.pop();
-            queueFrontReciver.push(lobbies);
-            break;
+        // case VIEW_LOBBIES_EVENT:
+        //     LobbyAction msj(1, NULL, NULL);
+        //     queueSender.push(&msj);
+        //     auto lobbies = queueReciver.pop();
+        //     queueFrontReciver.push(lobbies);
+        //     break;
         
-        case CHOOSE_LOBBY_EVENT:
-            LobbyAction msj(2, mensaje[1], mensaje[2]);
-            queueSender.push(&msj);
-            break;
+        // case CHOOSE_LOBBY_EVENT:
+        //     LobbyAction msj(2, mensaje[1], mensaje[2]);
+        //     queueSender.push(&msj);
+        //     break;
 
-        case CREATE_LOBBY_ONE_LOCAL_PLAYER_EVENT:
-            LobbyAction msj(3, mensaje[1]);
-            queueSender.push(&msj);
-            lobby_created = queueReciver.pop();
-            if (lobby_created) {
-                queueFrontReciver(ok?);
-            }
-            break;
+        // case CREATE_LOBBY_ONE_LOCAL_PLAYER_EVENT:
+        //     LobbyAction msj(3, mensaje[1]);
+        //     queueSender.push(&msj);
+        //     lobby_created = queueReciver.pop();
+        //     if (lobby_created) {
+        //         queueFrontReciver(ok?);
+        //     }
+        //     break;
 
-        case CUSTOMIZE_PLAYER_EVENT:
-            CustomizePlayer msj(true, NULL, NULL);
-            queueSender.push(&msj);
-            auto colors = queueReciver.pop();
-            queueFrontReciver.push(colors);
-            break;
+        // case CUSTOMIZE_PLAYER_EVENT:
+        //     CustomizePlayer msj(true, NULL, NULL);
+        //     queueSender.push(&msj);
+        //     auto colors = queueReciver.pop();
+        //     queueFrontReciver.push(colors);
+        //     break;
 
-        case PLAYER_CUSTOMIZED_EVENT:
-            CustomizePlayer msj(false, mensaje[1], mensaje[2]);
-            queueSender.push(&msj);
-            break;
+        // case PLAYER_CUSTOMIZED_EVENT:
+        //     CustomizePlayer msj(false, mensaje[1], mensaje[2]);
+        //     queueSender.push(&msj);
+        //     break;
 
-        default:
-            break;
-        }
+        // default:
+        //     break;
+        // }
 
     }
     
