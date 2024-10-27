5b9faa6031f4dbfea4ff3e1763f14391c043ec88
FacuGerez <facundogerezuba@gmail.com>
2024-10-27 18:59:52 -0300

    fix problems with change pointer and compiling succed

--- client/client_protocol.cpp
+++ client/client_protocol.cpp
@@ -5,23 +5,47 @@
 ClientProtocol::ClientProtocol(Socket& skt): ProtocoloCommon(skt) {
     recv_handlers[GenericMsg::typeMsg::RESPONSE_SERVER] = {
             {ResponseServerMsg::responseType::EVERYTHING_WENT_WELL,
-             [this](GenericMsg* msg) { this->handle_recv(dynamic_cast<EverythingOkey*>(msg)); }},
-            {ResponseServerMsg::responseType::SOMETHING_WAS_WRONG,
-             [this](GenericMsg* msg) { this->handle_recv(dynamic_cast<SomethingWrong*>(msg)); }}};
+             [this]() {
+                 EverythingOkey* msg;
+                 this->handle_recv(&msg);
+                 return msg;
+             }},
+            {ResponseServerMsg::responseType::SOMETHING_WAS_WRONG, [this]() {
+                 SomethingWrong* msg;
+                 this->handle_recv(&msg);
+                 return msg;
+             }}};
+
     recv_handlers[GenericMsg::typeMsg::LOBBY_MESSAGE] = {
             {LobbyMsg::lobbyType::EXAMPLE,
-             [this](GenericMsg* msg) { this->handle_recv(dynamic_cast<ExampleMsg*>(msg)); }},
-            {LobbyMsg::lobbyType::LOBBY_LIST,
-             [this](GenericMsg* msg) { this->handle_recv(dynamic_cast<LobbyListMsg*>(msg)); }},
-    };
-    recv_handlers[GenericMsg::typeMsg::GAME_MESSAGE] = {
-            {GameMsg::gameType::PLAYER_INFO,
-             [this](GenericMsg* msg) { this->handle_recv(dynamic_cast<PlayerInfoMsg*>(msg)); }},
-            {GameMsg::gameType::FINISH_GAME,
-             [this](GenericMsg* msg) { this->handle_recv(dynamic_cast<FinishGameMsg*>(msg)); }},
-            {GameMsg::gameType::WINNER,
-             [this](GenericMsg* msg) { this->handle_recv(dynamic_cast<WinnerMsg*>(msg)); }},
-    };
+             [this]() {
+                 ExampleMsg* msg;
+                 this->handle_recv(&msg);
+                 return msg;
+             }},
+            {LobbyMsg::lobbyType::LOBBY_LIST, [this]() {
+                 LobbyListMsg* msg;
+                 this->handle_recv(&msg);
+                 return msg;
+             }}};
+
+    recv_handlers[GenericMsg::typeMsg::GAME_MESSAGE] = {{GameMsg::gameType::PLAYER_INFO,
+                                                         [this]() {
+                                                             PlayerInfoMsg* msg;
+                                                             this->handle_recv(&msg);
+                                                             return msg;
+                                                         }},
+                                                        {GameMsg::gameType::FINISH_GAME,
+                                                         [this]() {
+                                                             FinishGameMsg* msg;
+                                                             this->handle_recv(&msg);
+                                                             return msg;
+                                                         }},
+                                                        {GameMsg::gameType::WINNER, [this]() {
+                                                             WinnerMsg* msg;
+                                                             this->handle_recv(&msg);
+                                                             return msg;
+                                                         }}};
 }
 
 void ClientProtocol::send(GenericMsg* msg) { msg->accept_send(*this); }
@@ -29,9 +53,7 @@ void ClientProtocol::send(GenericMsg* msg) { msg->accept_send(*this); }
 GenericMsg* ClientProtocol::receive() {
     uint8_t header = recv_u_int8_t();
     uint8_t second_header = recv_u_int8_t();
-    GenericMsg* msg = nullptr;
-    recv_handlers[static_cast<GenericMsg::typeMsg>(header)][second_header](msg);
-    return msg;
+    return recv_handlers[static_cast<GenericMsg::typeMsg>(header)][second_header]();
 }
 
 void ClientProtocol::sendCabecera(const GenericMsg& msg) {
@@ -41,9 +63,9 @@ void ClientProtocol::sendCabecera(const GenericMsg& msg) {
     send_u_int8_t(second_header);
 }
 
-void ClientProtocol::handle_recv(EverythingOkey* msg) { msg = new EverythingOkey(); }
+void ClientProtocol::handle_recv(EverythingOkey** msg) { *msg = new EverythingOkey(); }
 
-void ClientProtocol::handle_recv(SomethingWrong* msg) { msg = new SomethingWrong(); }
+void ClientProtocol::handle_recv(SomethingWrong** msg) { *msg = new SomethingWrong(); }
 
 void ClientProtocol::handle_send(const ExampleMsg& msg) {
     sendCabecera(msg);
@@ -61,12 +83,12 @@ void ClientProtocol::handle_send(const ExitMsg& msg) { sendCabecera(msg); }
 
 void ClientProtocol::handle_send(const StartGameMsg& msg) { sendCabecera(msg); }
 
-void ClientProtocol::handle_recv(ExampleMsg* msg) {
+void ClientProtocol::handle_recv(ExampleMsg** msg) {
     std::string data = recv_string();
-    msg = new ExampleMsg(data);
+    *msg = new ExampleMsg(data);
 }
 
-void ClientProtocol::handle_recv(LobbyListMsg* msg) { msg = new LobbyListMsg(); }
+void ClientProtocol::handle_recv(LobbyListMsg** msg) { *msg = new LobbyListMsg(); }
 
 void ClientProtocol::handle_send(const CustomizedPlayerInfoMsg& msg) { sendCabecera(msg); }
 
@@ -86,8 +108,8 @@ void ClientProtocol::handle_send(const PlayDeadMsg& msg) { sendCabecera(msg); }
 
 void ClientProtocol::handle_send(const ShootMsg& msg) { sendCabecera(msg); }
 
-void ClientProtocol::handle_recv(const PlayerInfoMsg* msg) { msg = new PlayerInfoMsg(); }
+void ClientProtocol::handle_recv(PlayerInfoMsg** msg) { *msg = new PlayerInfoMsg(); }
 
-void ClientProtocol::handle_recv(const FinishGameMsg* msg) { msg = new FinishGameMsg(); }
+void ClientProtocol::handle_recv(FinishGameMsg** msg) { *msg = new FinishGameMsg(); }
 
-void ClientProtocol::handle_recv(const WinnerMsg* msg) { msg = new WinnerMsg(); }
+void ClientProtocol::handle_recv(WinnerMsg** msg) { *msg = new WinnerMsg(); }
--- client/client_protocol.h
+++ client/client_protocol.h
@@ -9,8 +9,7 @@
 
 class ClientProtocol: public ProtocoloCommon, public Handler {
 private:
-    std::map<GenericMsg::typeMsg, std::map<uint8_t, std::function<void(GenericMsg*)>>>
-            recv_handlers;
+    std::map<GenericMsg::typeMsg, std::map<uint8_t, std::function<GenericMsg*()>>> recv_handlers;
 
     void sendCabecera(const GenericMsg& msg);
 
@@ -25,8 +24,8 @@ public:
     using Handler::handle_send;
 
     // RESPONSE SERVER MESSAGES
-    virtual void handle_recv(EverythingOkey* msg) override;
-    virtual void handle_recv(SomethingWrong* msg) override;
+    virtual void handle_recv(EverythingOkey** msg) override;
+    virtual void handle_recv(SomethingWrong** msg) override;
 
     // LOBBY MESSAGES
     virtual void handle_send(const ExampleMsg& msg) override;
@@ -37,85 +36,25 @@ public:
     virtual void handle_send(const StartGameMsg& msg) override;
 
 
-    virtual void handle_recv(ExampleMsg* msg) override;
-    virtual void handle_recv(LobbyListMsg* msg) override;
+    virtual void handle_recv(ExampleMsg** msg) override;
+    virtual void handle_recv(LobbyListMsg** msg) override;
 
     // CUSTOMIZED MESSAGES
-    virtual void handle_send(const CustomizedPlayerInfoMsg& msg);
+    virtual void handle_send(const CustomizedPlayerInfoMsg& msg) override;
 
     // GAME MESSAGES
-    virtual void handle_send(const PickupDropItemMsg& msg);
-    virtual void handle_send(const StartMoveLeftMsg& msg);
-    virtual void handle_send(const StopMoveLeftMsg& msg);
-    virtual void handle_send(const StartMoveRightMsg& msg);
-    virtual void handle_send(const StopMoveRightMsg& msg);
-    virtual void handle_send(const JumpMsg& msg);
-    virtual void handle_send(const PlayDeadMsg& msg);
-    virtual void handle_send(const ShootMsg& msg);
+    virtual void handle_send(const PickupDropItemMsg& msg) override;
+    virtual void handle_send(const StartMoveLeftMsg& msg) override;
+    virtual void handle_send(const StopMoveLeftMsg& msg) override;
+    virtual void handle_send(const StartMoveRightMsg& msg) override;
+    virtual void handle_send(const StopMoveRightMsg& msg) override;
+    virtual void handle_send(const JumpMsg& msg) override;
+    virtual void handle_send(const PlayDeadMsg& msg) override;
+    virtual void handle_send(const ShootMsg& msg) override;
 
-    virtual void handle_recv(const PlayerInfoMsg* msg);
-    virtual void handle_recv(const FinishGameMsg* msg);
-    virtual void handle_recv(const WinnerMsg* msg);
-
-    /*
-    ESTO LO GUARDO DE EJEMPLO QUE TENEMOS QUE HACER PARA EL RESTO DE LOS MENSAJES
-
-    void handle_send(ClientExampleMsg1& msg) override {
-        // mando header
-        uint8_t header = msg.get_header();
-        send_uint8_t(header);
-        // mando longitud del string
-        uint16_t dataSize = msg.get_data().size();
-        send_uint16_t(dataSize);
-        // mando string
-        auto data = msg.get_data();
-        send_string(data);
-        return;
-    }
-
-    void handle_send(ClientExampleMsg2& msg) override {
-        // mando header
-        uint8_t header = msg.get_header();
-        send_uint8_t(header);
-        // mando data
-        uint8_t data = msg.get_data();
-        send_uint8_t(data);
-        return;
-    }
-
-    */
-
-    /*
-    HAY UNA FORMA MAS FACIL PARA ESTO, POR AHORA IGUAL QUIERO QUE FUNCIONE
-
-    GenericMsg* receive() override {
-        uint8_t header = recv_uint8_t(); // recibo el header que representa al mensaje
-        GenericMsg* msg = generate_msg(header); // genero el mensaje dado el header
-        msg->accept_recv(*this);
-        return msg;
-    }
-
-    void handle_recv(ServerExampleMsg1& msg) override {
-        uint16_t dataSize = recv_uint16_t();
-        std::string data = recv_string(dataSize);
-        msg.set_data(data);
-    }
-
-    GenericMsg* generate_msg(uint8_t header) {
-        // pongo todos los case pero solo estaria recibiendo un solo tipo de mensaje
-        switch (header) {
-            case GenericMsg::SERVER_EXAMPLE_MSG1_H:
-                return new ServerExampleMsg1("");
-            case GenericMsg::CLIENT_EXAMPLE_MSG1_H:
-                return new ClientExampleMsg1("");
-            case GenericMsg::CLIENT_EXAMPLE_MSG2_H:
-                return new ClientExampleMsg2(0);
-            default:
-                return nullptr;
-        }
-    }
-
-    */
+    virtual void handle_recv(PlayerInfoMsg** msg) override;
+    virtual void handle_recv(FinishGameMsg** msg) override;
+    virtual void handle_recv(WinnerMsg** msg) override;
 };
 
 #endif  // CLIENT_PROTOCOL_H
