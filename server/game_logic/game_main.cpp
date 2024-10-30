0d8da4c9b17cea9fea152233e52bbd421ad8cd5e
FacuGerez <facundogerezuba@gmail.com>
2024-10-30 07:28:46 -0300

    Refactor GameMain to use a queue for message and read this execute double dispatch

--- server/game_logic/game_main.cpp
+++ server/game_logic/game_main.cpp
@@ -1,29 +1,43 @@
 #include "game_main.h"
 
-GameMain::GameMain() {}
+GameMain::GameMain(Queue<GenericMsg*>&): q(q) {
+    // se puede mejorar esto haciendo que capaz reciba antes el stage por referencia
+    // o capaz en el lobby cuando se elige el mapa, ahi se defina el stage antes jeje
+    CSVWriter::write_map("main_map.csv");
+    stage = new Stage("main_map.csv");
+}
 
 int GameMain::run() {
-    CSVWriter::write_map("main_map.csv");
-    Stage stage("main_map.csv");
     Coordinate coordinate_a(30, 36);
 
-    Player player_a(coordinate_a, stage, 2);
-    stage.draw_player(player_a);
+    Player player_a(coordinate_a, *stage, 2);
+    stage->draw_player(player_a);
     while (true) {
-        // Aca hay que try_popear de una cola de mensajes
-        // GenericMsg msg = q.pop();
-        // msg.handle_read(*this);
+        // Aca hay que try_popear de la cola de mensajes
+        GenericMsg* msg;
+        /* ESTO SE PUEDE HACER ASI
+        // por cada uno de los mensajes recibidos, se hace un accept_read
+        while (q.try_pop(msg)) {
+            msg->accept_read(*this); // Esto hace un mappeo automatico Martin el cual
+            // podes pensarlo como que accept_read ----> handle_read (del GameMain)
+        }*/
+        // O SE PUEDE HACER ASI
+        q.try_pop(msg);  // lees es una unica iteracion un solo mensaje o nada si no tenes nada en
+                         // la queue
+        msg->accept_read(*this);
         std::string command;
         std::cin >> command;
-        stage.delete_player_from_stage(player_a);  // Borro su dibujo viejo
+        stage->delete_player_from_stage(player_a);  // Borro su dibujo viejo
         player_a.add_action(command);
         player_a.update();
-        stage.draw_player(player_a);
-        stage.update();
-        stage.print();
+        stage->draw_player(player_a);
+        stage->update();
+        stage->print();
     }
     return 0;
 }
+
+// El accept_read te mappea a estas funciones de aca abajo
 
 void GameMain::handle_read(const PickupDropMsg& msg) {
     // No tengo idea que se hace con el pick up drop
--- server/game_logic/game_main.h
+++ server/game_logic/game_main.h
@@ -4,14 +4,23 @@
 #include <iostream>
 #include <string>
 
+#include "../../common/queue.h"
+//#include "../../common/thread.h"
 #include "../../common/messages/generic_msg.cpp"
+#include "../../common/messages/generic_msg.h"
 #include "../../common/messages/handler_read.h"
 #include "map/csv_writer.h"
 #include "map/stage.h"
 #include "player/player.h"
 
 class GameMain: public HandlerReader {
+private:
+    Stage* stage;
+    Queue<GenericMsg*>& q;
+
 public:
+    explicit GameMain(Queue<GenericMsg*>& q);
+
     int run();
 
     using HandlerReader::handle_read;
