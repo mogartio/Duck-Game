6e4a8306959b25c551245de5c14f2df47e73cf3c
cpiccin <cpiccin@fi.uba.ar>
2024-11-01 13:25:35 -0300

    Experimentacion con cmake

--- server/server.cpp
+++ server/server.cpp
@@ -3,16 +3,12 @@
 #include <list>
 #include <utility>
 
-#include "game_logic/game_main.h"
 // is_testing sirve para testear cosas del juego a mano
 Server::Server(const char* port, bool is_testing):
         srv(port), clients(), recv_queue(100), send_queues(), is_testing(is_testing) {}
 
 void Server::run() {
-    // Queue<GenericMsg*> q(1000);
-    // GameMain game(q, "juancito", "pedrito", is_testing);
-    // game.start();  // esto deberia ser un thread ya
-    // // pongo a correr el acceptor
+
     Acceptor acceptor(std::move(srv), clients, recv_queue, send_queues);
     acceptor.start();
     // pongo a correr el thread que lee input (TEMPORAL)
@@ -36,5 +32,4 @@ void Server::run() {
     acceptor.stop();
     acceptor.join();
     read_input_t.join();
-    // game.join();
 }
