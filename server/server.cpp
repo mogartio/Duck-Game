49fcc6238e2d1ac44d228ed15f5f2683cece5582
Candela <piccincandela@gmail.com>
2024-11-02 22:10:03 -0300

    aaa

--- server/server.cpp
+++ server/server.cpp
@@ -9,44 +9,35 @@ Server::Server(const char* port, bool is_testing):
         srv(port), clients(), recv_queue(100), send_queues(), is_testing(is_testing) {}
 
 void Server::run() {
-<<<<<<< HEAD
 
-=======
-    GameMain game(recv_queue, "juancito", "pedrito", is_testing, send_queues);
-    game.start();
-    // pongo a correr el acceptor
->>>>>>> main
     Acceptor acceptor(std::move(srv), clients, recv_queue, send_queues);
     acceptor.start();
     ReadInput read_input_t;
-<<<<<<< HEAD
     read_input_t.start();
-=======
-    // read_input_t.start();
-    // read_input_t.start();
->>>>>>> main
 
-    WinnerMsg msg1("Candela");
-    UpdatedPlayerInfoMsg msg2("Candela", std::make_pair(1, 1), 1, 1);
 
-    std::vector<uint16_t> map = {5,5,5,0,0,2,0,0,0,0,0,0,0,0,0,0,5,5,5,5,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+    std::vector<uint16_t> map = {
+        0,0,0,0,0,0,0,
+        0,0,0,2,0,0,0,
+        0,0,5,5,5,5,0,
+        0,0,0,5,5,0,0,
+        0,0,0,0,0,0,0,
+        0,0,0,0,0,0,0};
     uint16_t fil = 6;
     uint16_t col = 7;
     SendMapMsg msg3(map, fil, col);
-
+    int n = 1;
 
     while (read_input_t.is_alive()) {
         std::list<GenericMsg*> msgs;
         GenericMsg* msg;
-        while (recv_queue.try_pop(msg)) {
-<<<<<<< HEAD
+        if (n == 1) {
             msgs.push_back(&msg3);
-=======
-            std::cout << "Mensaje recibido con header: 0x" << std::hex << std::setw(2)
-                      << std::setfill('0') << static_cast<int>(msg->get_header()) << std::endl;
-            msgs.push_back(&msg1);
-            msgs.push_back(&msg2);
->>>>>>> main
+            send_queues.broadcast(msgs);
+            break;
+        }
+        while (recv_queue.try_pop(msg)) {
+            
         }
         send_queues.broadcast(msgs);
     }
