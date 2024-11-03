8e29f26ee40f53e9e5df5fe4f88e105b82343dc5
FacuGerez <facundogerezuba@gmail.com>
2024-11-03 11:17:26 -0300

    Compiling and deleted segmentation fault server side

--- server/server.cpp
+++ server/server.cpp
@@ -9,15 +9,12 @@ Server::Server(const char* port, bool is_testing):
         srv(port), clients(), recv_queue(100), send_queues(), is_testing(is_testing) {}
 
 void Server::run() {
-    GameMain game(recv_queue, "juancito", "pedrito", is_testing, send_queues);
-    game.start();
     // pongo a correr el acceptor
     Acceptor acceptor(std::move(srv), clients, recv_queue, send_queues);
     acceptor.start();
     // pongo a correr el thread que lee input (TEMPORAL)
     ReadInput read_input_t;
-    // read_input_t.start();
-    // read_input_t.start();
+    read_input_t.start();
 
     WinnerMsg msg1("Candela");
     UpdatedPlayerInfoMsg msg2("Candela", std::make_pair(1, 1), 1, 1);
