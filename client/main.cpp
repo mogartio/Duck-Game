e0df6e6d57c2fd1d8af2682a7b5e080fa9518b6f
FacuGerez <facundogerezuba@gmail.com>
2024-11-04 08:40:45 -0300

    FEATURE: implement interactive lobby commands with user input handling

--- client/main.cpp
+++ client/main.cpp
@@ -1,4 +1,6 @@
+#include <iomanip>  // Incluir el encabezado <iomanip> para std::setw y std::setfill
 #include <iostream>
+#include <sstream>  // Incluir el encabezado <sstream>
 
 #include "../common/messages/generic_msg.h"
 #include "../common/socket/socket.h"
@@ -18,42 +20,47 @@ int main(int argc, char const* argv[]) {
 
     ClientProtocol protocol(skt);
 
-    ViewLobbiesMsg msg;
+    std::string input_line;
+    std::stringstream input;
+    int directive;
+    while (true) {
+        std::getline(std::cin, input_line);   // Leer la entrada en una cadena de texto
+        std::stringstream input(input_line);  // Insertar la cadena en el stringstream
+        input >> directive;
+        GenericMsg* msg;
 
-    msg.accept_send(protocol);
+        switch (directive) {
+            case 1:
+                std::cout << "Preguntando por lobbies" << std::endl;
+                msg = new ViewLobbiesMsg();
+                break;
+            case 2:
+                std::cout << "Creando lobby con nombre de player Player1" << std::endl;
+                msg = new CreateLobbyMsg("player1");
+                break;
+            case 3:
+                std::cout << "Eligiendo lobby 1 con nombre de player Player2" << std::endl;
+                msg = new ChooseLobbyMsg(1, "player2");
+                break;
+            case 4:
+                std::cout << "Saliendo del lobby con nombre de player Player2" << std::endl;
+                msg = new ExitFromLobbyMsg("player2");
+                break;
+            case 5:
+                std::cout << "Iniciando juego" << std::endl;
+                msg = new StartGameMsg();
+                break;
+            default:
+                std::cout << "Termino el programa" << std::endl;
+                return 0;
+        }
 
-    GenericMsg* msg_recv = protocol.receive();
+        msg->accept_send(protocol);
+        GenericMsg* msg_recv = protocol.receive();
 
-    std::cout << "Mensaje recibido con header: 0x" << std::hex << std::setw(2) << std::setfill('0')
-              << static_cast<int>(msg_recv->get_header()) << std::endl;
-
-    CreateLobbyMsg msg2("player1");
-
-    msg2.accept_send(protocol);
-
-    GenericMsg* msg_recv2 = protocol.receive();
-
-    std::cout << "Mensaje recibido con header: 0x" << std::hex << std::setw(2) << std::setfill('0')
-              << static_cast<int>(msg_recv2->get_header()) << std::endl;
-
-    ChooseLobbyMsg msg3(1, "player2");
-
-    msg3.accept_send(protocol);
-
-    GenericMsg* msg_recv3 = protocol.receive();
-
-    std::cout << "Mensaje recibido con header: 0x" << std::hex << std::setw(2) << std::setfill('0')
-              << static_cast<int>(msg_recv3->get_header()) << std::endl;
-
-    ExitFromLobbyMsg msg4("player2");
-
-    msg4.accept_send(protocol);
-
-    GenericMsg* msg_recv4 = protocol.receive();
-
-    std::cout << "Mensaje recibido con header: 0x" << std::hex << std::setw(2) << std::setfill('0')
-              << static_cast<int>(msg_recv4->get_header()) << std::endl;
-
+        std::cout << "Mensaje recibido con header: 0x" << std::hex << std::setw(2)
+                  << std::setfill('0') << static_cast<int>(msg_recv->get_header()) << std::endl;
+    }
 
     /*
 
