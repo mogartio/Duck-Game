c8641e212af458efeaf6f8ba01da59783aba6b7a
Candela <piccincandela@gmail.com>
2024-11-06 23:17:58 -0300

    Agrego boton QUIT que cierra la app y manejo la conexion a un socket, si se logra conectar emite la senial para cambiar a la proxima screen, si no tira advertencias y sigue intentando

--- client/menu/connection_screen.cpp
+++ client/menu/connection_screen.cpp
@@ -7,8 +7,11 @@
 #include <QLineEdit>
 #include <qfontdatabase.h>
 #include <QPushButton>
-#include <QDebug>
-
+#include <QDebug> 
+#include <iostream>
+#include <QSound>
+#include <string>
+#include <iostream>
 ConnectionScreen::ConnectionScreen(Queue<std::unique_ptr<GenericMsg>>& send_queue, Queue<std::unique_ptr<GenericMsg>>& recv_queue) : send_queue(send_queue), recv_queue(recv_queue) {
     setWindowState(Qt::WindowFullScreen); // Set window to full-screen mode
 
@@ -16,23 +19,25 @@ ConnectionScreen::ConnectionScreen(Queue<std::unique_ptr<GenericMsg>>& send_queu
     setFocusPolicy(Qt::StrongFocus);
 
     // Create ParallaxBackground layers
-    layer0 = new ParallaxBackground(this, "client/img_src/front_window_background/parallax_background_layer_1.png", 0.5f);
-    layer1 = new ParallaxBackground(this, "client/img_src/front_window_background/parallax_background_layer_2.png", 0.75f);
-    layer2 = new ParallaxBackground(this, "client/img_src/front_window_background/parallax_background_layer_3.png", 1.0f);
-    layer3 = new ParallaxBackground(this, "client/img_src/front_window_background/parallax_background_layer_4.png", 1.25f);
+    layer0 = new ParallaxBackground(this, "client/menu/assets/parallax_background_layer_1.png", 0.5f);
+    layer1 = new ParallaxBackground(this, "client/menu/assets/parallax_background_layer_2.png", 0.75f);
+    layer2 = new ParallaxBackground(this, "client/menu/assets/parallax_background_layer_3.png", 1.0f);
+    layer3 = new ParallaxBackground(this, "client/menu/assets/parallax_background_layer_4.png", 1.25f);
 
     layer0->start();
     layer1->start();
     layer2->start(); 
     layer3->start();
 
+    keyPressSound = new QSound("client/menu/assets/Retro3.wav", this);
+
     // Center of the screen: (1920 / 2, 1080 / 2) = (960, 540)
     // Top-left corner of the rectangle: (960 - 500 / 2, 540 - 400 / 2) = (710, 340)
-    RoundedRectangle * baseRectangle = new RoundedRectangle(this, 650, 280, 620, 520, QColor(213, 226, 224, 100), QColor(213, 226, 224, 100));
+    RoundedRectangle * baseRectangle = new RoundedRectangle(this, 650, 280, 620, 560, QColor(213, 226, 224, 100), QColor(213, 226, 224, 100));
 
-    int fontId = QFontDatabase::addApplicationFont("client/text_font_src/PixelBook-Regular.ttf");
+    int fontId = QFontDatabase::addApplicationFont("client/menu/assets/HomeVideo-Regular.ttf");
     QString fontFamily = QFontDatabase::applicationFontFamilies(fontId).at(0);
-    QFont customFont(fontFamily);
+    customFont = QFont(fontFamily);
     
     QLabel *hostnameLabel = new QLabel("hostname", this);
     QLabel *portLabel = new QLabel("port", this);
@@ -46,8 +51,11 @@ ConnectionScreen::ConnectionScreen(Queue<std::unique_ptr<GenericMsg>>& send_queu
     baseRectangle->setParent(this);
 
     // Create the input fields
-    QLineEdit * hostname = new QLineEdit(this);
-    QLineEdit * port = new QLineEdit(this);
+    hostname = new QLineEdit(this);
+    port = new QLineEdit(this);
+    hostname->setGeometry(710, 400, 500, 50);
+    port->setGeometry(710, 500, 500, 50);
+
     hostname->setStyleSheet(
         "QLineEdit {"
         "background-color:rgba(206, 212, 218, 255);"     // Light gray background
@@ -75,8 +83,7 @@ ConnectionScreen::ConnectionScreen(Queue<std::unique_ptr<GenericMsg>>& send_queu
         "background-color: rgba(189, 194, 199, 255);"     // Slightly darker on hover
         "}"
         );
-    hostname->setGeometry(710, 500, 500, 50);
-    port->setGeometry(710, 400, 500, 50);
+
     hostname->setFont(customFont);
     port->setFont(customFont);
 
@@ -88,7 +95,7 @@ ConnectionScreen::ConnectionScreen(Queue<std::unique_ptr<GenericMsg>>& send_queu
         "color: #ced4da;"                     // White text
         "font-size: 42px;"                  // Font size
         "border: 0px solid #555555;"        // Border color and size
-        "border-radius: 20px;"              // Rounded corners
+        "border-radius: 15px;"              // Rounded corners
         "padding: 10px;"                    // Padding inside the button
         "text-align: center;"               // Center the text horizontally
         "}"
@@ -100,12 +107,29 @@ ConnectionScreen::ConnectionScreen(Queue<std::unique_ptr<GenericMsg>>& send_queu
 
     connectButton->setGeometry(845, 600, 240, 100);
 
-        // Connect the button's clicked signal to a slot
-    connect(connectButton, &QPushButton::clicked, this, []() {
-        qDebug() << "Connect button clicked";
-    });
-    
+    // Connect the button's clicked signal to a slot
+    connect(connectButton, &QPushButton::clicked, this, &ConnectionScreen::onConnectButtonClicked);
 
+    // Create the quit button
+    QPushButton *quitButton = new QPushButton("QUIT", this);
+    quitButton->setStyleSheet(
+        "QPushButton {"
+        "background-color: rgba(240, 140, 0, 100);"        // Blue background
+        "color: #ced4da;"                     // White text
+        "font-size: 42px;"                  // Font size
+        "border: 0px solid #555555;"        // Border color and size
+        "border-radius: 15px;"              // Rounded corners
+        "padding: 10px;"                    // Padding inside the button
+        "text-align: center;"               // Center the text horizontally
+        "}"
+        "QPushButton:hover {"
+        "background-color: rgba(232, 89, 12, 100);"        // Slightly darker on hover
+        "}"
+    );
+    quitButton->setFont(customFont);
+    quitButton->setGeometry(880, 710, 160, 70);
+
+    connect(quitButton, &QPushButton::clicked, this, &ConnectionScreen::onQuitButtonClicked); 
 }
 
 void ConnectionScreen::resizeEvent(QResizeEvent *event) {
@@ -116,4 +140,54 @@ void ConnectionScreen::resizeEvent(QResizeEvent *event) {
     layer2->resizeImages();
     layer3->resizeImages();
     update();
+}
+
+void ConnectionScreen::onConnectButtonClicked() {
+    keyPressSound->play();
+    std::cout << "Connect button clicked" << std::endl;
+    if (hostname->text().isEmpty() || port->text().isEmpty()) {
+        drawEmptyFieldsError();
+        return;
+    }
+    try {
+        std::string hostnameStr = hostname->text().toStdString();
+        std::string portStr = port->text().toStdString();
+        Socket skt(hostnameStr.c_str(), portStr.c_str());
+        // successful connection
+        emit switchToMainMenuScreen();
+    } catch (const std::exception &e) {
+        drawConnectionRefusedError();
+    }
+}
+
+void ConnectionScreen::onQuitButtonClicked() {
+    keyPressSound->play();
+    std::cout << "Quit button clicked" << std::endl;
+    emit quitApplication();
+}
+
+void ConnectionScreen::drawEmptyFieldsError() {
+    // Create a QLabel to display the error message
+    QLabel *errorLabel = new QLabel("Please fill in all fields", this);
+    errorLabel->setStyleSheet("QLabel { color: #800404; font-size: 24px; }");
+    errorLabel->setFont(QFont(customFont));
+    errorLabel->setGeometry(710, 550, 500, 50);
+    errorLabel->show();
+    // Remove the error message after 2 seconds
+    QTimer::singleShot(2000, errorLabel, &QObject::deleteLater);
+}
+
+void ConnectionScreen::drawConnectionRefusedError() {
+    // Create a QLabel to display the error message
+    QLabel *errorLabel = new QLabel("Connection refused", this);
+    errorLabel->setStyleSheet("QLabel { color: #800404; font-size: 24px; }");
+    errorLabel->setFont(QFont(customFont));
+    errorLabel->setGeometry(710, 550, 500, 50);
+    errorLabel->show();
+    // Remove the error message after 2 seconds
+    QTimer::singleShot(2000, errorLabel, &QObject::deleteLater);
+}
+
+Socket* ConnectionScreen::getSocket() {
+    return socket;
 }
--- client/menu/connection_screen.h
+++ client/menu/connection_screen.h
@@ -4,24 +4,42 @@
 #include <QWidget>
 #include <QPainter>
 #include <QResizeEvent>
+#include <QLineEdit>
 #include "parallax_background.h"
 #include "../../common/queue.h"
 #include "../../common/messages/generic_msg.h"
-
+#include "../../common/socket/socket.h"
+#include <QSound>
+#include <QFontDatabase>
 class ConnectionScreen : public QWidget {
     Q_OBJECT
 
 public: 
     ConnectionScreen(Queue<std::unique_ptr<GenericMsg>>& send_queue, Queue<std::unique_ptr<GenericMsg>>& recv_queue);
+    Socket* getSocket();
+signals:
+    void switchToMainMenuScreen();
+    void quitApplication();
+
 
 protected:
     void resizeEvent(QResizeEvent *event) override;
+    void drawEmptyFieldsError();
+    void drawConnectionRefusedError();
+private slots:
+    void onConnectButtonClicked();
+    void onQuitButtonClicked();
     
 private:
+    Socket *socket;
     ParallaxBackground *layer0;
     ParallaxBackground *layer1;
     ParallaxBackground *layer2;
     ParallaxBackground *layer3;
+    QSound *keyPressSound;
+    QLineEdit *hostname;
+    QLineEdit *port;
+    QFont customFont;
     Queue<std::unique_ptr<GenericMsg>>& send_queue;
     Queue<std::unique_ptr<GenericMsg>>& recv_queue;
 };
