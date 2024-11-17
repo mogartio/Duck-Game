93b947c0fd1db2adf6371665309bcc851bb2781b
cpiccin <cpiccin@fi.uba.ar>
2024-11-16 22:23:52 -0300

    el main de client recibe la lista de players para el render

--- client/main.cpp
+++ client/main.cpp
@@ -18,8 +18,17 @@ int main(int argc, char *argv[]) {
     MainWindow mainWindow(nullptr, send_queue, recv_queue, client);
     mainWindow.show();
 
+    std::list<std::string> names;
+
     if (app.exec() == 0) {
         // se cerro ordenadamente y se tiene que iniciar el render del juego
+        GenericMsg* msg = recv_queue->pop();
+        if (msg->get_header() == GenericMsg::MsgTypeHeader::INFO_LOBBY_MSG) {
+            InfoLobbyMsg* info_msg = dynamic_cast<InfoLobbyMsg*>(msg);
+            for (auto& player: info_msg->get_players()) {
+                names.push_back(player.nombre);
+            }
+        }
     }
     return 0;
 }
