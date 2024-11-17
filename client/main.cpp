da4bda2e37a8870f7479e5468a5923998e9c3354
LuCalvert <lulicalvert@gmail.com>
2024-11-17 00:24:51 -0300

    intento de conexion fallido

--- client/main.cpp
+++ client/main.cpp
@@ -7,17 +7,19 @@
 #include "../common/messages/generic_msg.h"
 #include "../client/menu/create_game_screen.h"
 #include "../client/menu/connection_screen.h"
+#include "SDL/game/game.h"
+
 int main(int argc, char *argv[]) {
     QApplication app(argc, argv);
 
-    Queue<GenericMsg*>* send_queue = new Queue<GenericMsg*>(100);
-    Queue<GenericMsg*>* recv_queue = new Queue<GenericMsg*>(100);
+    Queue<GenericMsg*> send_queue(100);
+    Queue<GenericMsg*> recv_queue(100);
 
     Client* client = nullptr;
 
     std::list<std::string>* local_players = new std::list<std::string>();
 
-    MainWindow mainWindow(nullptr, send_queue, recv_queue, client, local_players);
+    MainWindow mainWindow(nullptr, &send_queue, &recv_queue, client, local_players);
     mainWindow.show();
 
 
@@ -30,5 +32,13 @@ int main(int argc, char *argv[]) {
     } else {
         return 1;
     }
+
+    local_players->push_back("");
+
+    std::string first_player = local_players->front();
+    local_players->pop_front();
+    Game game(send_queue, recv_queue, first_player, local_players->front());
+    game.play();
+    
     return 0;
 }
