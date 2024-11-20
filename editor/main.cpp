108e1a1767041c6d052d8757ea6f8bef375df437
Candela <piccincandela@gmail.com>
2024-11-20 18:34:38 -0300

    creo el ejecutable de editor

---
+++ editor/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <QApplication>
+#include "editor_main_window.h"
+
+int main(int argc, char const *argv[]) {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
