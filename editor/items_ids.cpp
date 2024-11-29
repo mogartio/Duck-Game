2664e9f12a402d8cde016777a97e6ffa23cb52ed
Candela <piccincandela@gmail.com>
2024-11-29 14:47:38 -0300

    fix editor, no se veian todos los helmets, era literal agregar dos palabras

--- editor/items_ids.cpp
+++ editor/items_ids.cpp
@@ -1,7 +1,7 @@
 #include "items_ids.h"
 
 namespace Id {
-    const std::set<int> weapons = {AK_47, BANANA, COWBOY_PISTOL, DUEL_PISTOL, SHOTGUN, GRENADE, MAGNUM, SNIPER, LASER_RIFLE, PEW_PEW_LASER, HELMET, CHEST};
+    const std::set<int> weapons = {AK_47, BANANA, COWBOY_PISTOL, DUEL_PISTOL, SHOTGUN, GRENADE, MAGNUM, SNIPER, LASER_RIFLE, PEW_PEW_LASER, HELMET, HELMET2, HELMET3, CHEST};
     const std::set<int> players = {PLAYER1, PLAYER2, PLAYER3, PLAYER4};
     const std::set<int> map_items = {EMPTY, ROCK, GRASS, COL};
 }
