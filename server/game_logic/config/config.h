8c1418ac42d5c754f609954957e486c3bf1f691b
Candela <piccincandela@gmail.com>
2024-11-28 23:02:44 -0300

    de nuevo saco info que quedo vieja y que no se saca del config

--- server/game_logic/config/config.h
+++ server/game_logic/config/config.h
@@ -26,20 +26,10 @@ private:
         player_falling_speed = config["player_falling_speed"].as<int>();
         player_jumping_height = config["player_jumping_height"].as<int>();
         armor_strength = config["armor_strength"].as<int>();
-        // columns_map = config["columns_map"].as<uint16_t>();
-        // rows_map = config["rows_map"].as<uint16_t>();
         explosion_counter = config["explosion_counter"].as<int>();
         explosion_range = config["explosion_range"].as<int>();
         bullet_size = config["bullet_size"].as<int>();
         rounds_per_cycle = config["rounds_per_cycle"].as<int>();
-        for (const auto& coords: config["weapon_spawn_sites"]) {
-            weapon_spawn_sites.push_back(
-                    std::tuple<int, int>(coords[0].as<int>(), coords[1].as<int>()));
-        }
-        for (const auto& coords: config["player_spawn_sites"]) {
-            player_spawn_sites.push_back(
-                    std::tuple<int, int>(coords[0].as<int>(), coords[1].as<int>()));
-        }
         int counter = 0;
         for (const auto& ids: config["mapId"]) {
             mapsId[ids.as<std::string>()] = counter;
@@ -52,12 +42,8 @@ public:
     int player_falling_speed;
     int player_jumping_height;
     int armor_strength;
-    uint16_t columns_map;
-    uint16_t rows_map;
     int rounds_per_cycle;
     std::map<std::string, uint16_t> mapsId;
-    std::vector<std::tuple<int, int>> weapon_spawn_sites;
-    std::vector<std::tuple<int, int>> player_spawn_sites;
     int explosion_counter;
     int explosion_range;
     int bullet_size;
