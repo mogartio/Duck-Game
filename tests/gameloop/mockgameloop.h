0e03a4031c1b0951bc62852cb074c41748d8c375
FacuGerez <facundogerezuba@gmail.com>
2024-11-30 12:29:51 -0300

    finalized started resources and tests movements in gtest

--- tests/gameloop/mockgameloop.h
+++ tests/gameloop/mockgameloop.h
@@ -1,54 +1,121 @@
 #include <gtest/gtest.h>
+#include <yaml-cpp/yaml.h>
+
 #include "../../server/game_logic/game.h"
 
 class GameResources {
 public:
     // Método para acceder a la instancia única del singleton
     static GameResources& GetInstance() {
-        static GameResources instance; // Instancia única
+        static GameResources instance;  // Instancia única
         return instance;
     }
 
-    bool IsResourcesLoaded() {
-        return resourcesLoaded;
-    }
+    bool IsResourcesLoaded() { return resourcesLoaded; }
 
     void LoadResources() {
         if (!resourcesLoaded) {
             std::cout << "Cargando recursos...\n";
             resourcesLoaded = true;
+            sendqueue1 = new Queue<std::shared_ptr<GenericMsg>>();
+            sendqueue2 = new Queue<std::shared_ptr<GenericMsg>>();
+            recv_queue = new Queue<std::shared_ptr<GenericMsg>>();
             ids = std::make_shared<std::set<uint>>();
             ids->insert(1);
             ids->insert(2);
-            senders.add(&sendqueue1,1);
-            senders.add(&sendqueue2,2);
+            senders.add(sendqueue1, 1);
+            senders.add(sendqueue2, 2);
             player_obs = std::make_shared<PlayerObserver>(senders, ids);
             player_names = {"player1", "player2"};
+            if (maps.empty()) {
+                LoadMaps();
+            }
         }
+    }
+
+    void freeNecesaryResources() {
+        for (auto& [_, player]: players) {
+            delete player;
+        }
+        players.clear();
+        delete sendqueue1;
+        delete sendqueue2;
+        delete recv_queue;
+        sendqueue1 = new Queue<std::shared_ptr<GenericMsg>>();
+        sendqueue2 = new Queue<std::shared_ptr<GenericMsg>>();
+        recv_queue = new Queue<std::shared_ptr<GenericMsg>>();
     }
 
     std::shared_ptr<GameLoop> UseResource(Map& map, Stage* current_stage) {
-        LoadResources();
+        if (!resourcesLoaded) {
+            return nullptr;
+        }
         std::vector<Coordinate> coordinates = map.get_players_spawn_sites();
         for (size_t i = 0; i < player_names.size(); i++) {
-            players[player_names[i]] = new Player(coordinates[i], i + 1, player_names[i], *player_obs.get());
+            players[player_names[i]] =
+                    new Player(coordinates[i], i + 1, player_names[i], *player_obs.get());
         }
-        return std::make_shared<GameLoop>(recv_queue, players, true);
+        return std::make_shared<GameLoop>(*recv_queue, players, false);
     }
 
-    void UnloadResources() {
+    void KillAll() {
         if (resourcesLoaded) {
-            std::cout << "Liberando recursos...\n";
+            std::cout << "Liberando todo...\n";
             resourcesLoaded = false;
+            delete sendqueue1;
+            delete sendqueue2;
             ids.reset();
             senders.remove_all();
             for (auto& [_, player]: players) {
                 delete player;
             }
+            players.clear();
+            player_names.clear();
+            for (auto& [_, map]: maps) {
+                delete map;
+            }
+            maps.clear();
+        }
+    };
+
+private:
+    void LoadMaps() {
+        std::string maps_folder_path = "tests_map";
+        for (const auto& entry: std::filesystem::directory_iterator(maps_folder_path)) {
+            std::string file_name = entry.path().filename().string();
+            if (file_name.find(".yaml") == std::string::npos) {
+                continue;
+            }
+            YAML::Node mapa_yaml = YAML::LoadFile(entry.path().string());
+            std::string new_map_name = mapa_yaml["map_name"].as<std::string>();
+            int rows = mapa_yaml["map_rows"].as<int>();
+            int columns = mapa_yaml["map_columns"].as<int>();
+            std::vector<Coordinate> players_spawn_sites;
+            std::vector<std::tuple<Coordinate, int>> items_spawn_sites;
+            std::vector<std::vector<int>> matrix;
+            for (const auto& spawn: mapa_yaml["map_player_spawn_sites"]) {
+                Coordinate spawn_point = Coordinate(spawn[0].as<int>(), spawn[1].as<int>());
+                players_spawn_sites.push_back(spawn_point);
+            }
+            for (const auto& spawn: mapa_yaml["map_weapon_spawn_sites"]) {
+                Coordinate spawn_point = Coordinate(spawn[0].as<int>(), spawn[1].as<int>());
+                int item_id = spawn[2].as<uint>();
+                items_spawn_sites.push_back(std::make_tuple(spawn_point, item_id));
+            }
+            for (const auto& row: mapa_yaml["map_matrix"]) {
+                std::vector<int> row_vector;
+                for (const auto& cell: row) {
+                    row_vector.push_back(cell.as<int>());
+                }
+                matrix.push_back(row_vector);
+            }
+            Map* new_map = new Map(new_map_name, rows, columns, matrix, players_spawn_sites,
+                                   items_spawn_sites);
+            maps[new_map_name] = new_map;
         }
     }
-private:
-    GameResources() = default; // Constructor privado
+
+    GameResources() = default;  // Constructor privado
     ~GameResources() = default;
 
     // Deshabilitar copia y movimiento
@@ -56,23 +123,27 @@ private:
     GameResources& operator=(const GameResources&) = delete;
 
     bool resourcesLoaded = false;
-    // Lo que no se necesita
-    SendQueuesMonitor<std::shared_ptr<GenericMsg>> senders;
-    std::shared_ptr<PlayerObserver> player_obs;
 
     // Lo que se necesita y se necesita getters
-    MapManager map_manager;
+    std::shared_ptr<PlayerObserver> player_obs;
+    SendQueuesMonitor<std::shared_ptr<GenericMsg>> senders;
     std::shared_ptr<std::set<uint>> ids;
-    Queue<std::shared_ptr<GenericMsg>> recv_queue;
-    Queue<std::shared_ptr<GenericMsg>> sendqueue1;
-    Queue<std::shared_ptr<GenericMsg>> sendqueue2;
+    Queue<std::shared_ptr<GenericMsg>>* recv_queue;
+    Queue<std::shared_ptr<GenericMsg>>* sendqueue1;
+    Queue<std::shared_ptr<GenericMsg>>* sendqueue2;
     std::vector<std::string> player_names;
     std::map<std::string, Player*> players;
+    std::map<std::string, Map*> maps;
 
 public:
-    MapManager& GetMapManager() {
-        return std::ref(map_manager);
+    std::shared_ptr<PlayerObserver> GetPlayerObserver() {
+        if (!resourcesLoaded) {
+            return nullptr;
+        }
+        return player_obs;
     }
+
+    SendQueuesMonitor<std::shared_ptr<GenericMsg>>& GetSenders() { return std::ref(senders); }
 
     std::shared_ptr<std::set<uint>> GetIds() {
         if (!resourcesLoaded) {
@@ -81,17 +152,11 @@ public:
         return ids;
     }
 
-    Queue<std::shared_ptr<GenericMsg>>& GetRecvQueue() {
-        return std::ref(recv_queue);
-    }
+    Queue<std::shared_ptr<GenericMsg>>& GetRecvQueue() { return std::ref(*recv_queue); }
 
-    Queue<std::shared_ptr<GenericMsg>>& GetSendQueue1() {
-        return std::ref(sendqueue1);
-    }
+    Queue<std::shared_ptr<GenericMsg>>& GetSendQueue1() { return std::ref(*sendqueue1); }
 
-    Queue<std::shared_ptr<GenericMsg>>& GetSendQueue2() {
-        return std::ref(sendqueue2);
-    }
+    Queue<std::shared_ptr<GenericMsg>>& GetSendQueue2() { return std::ref(*sendqueue2); }
 
     std::vector<std::string> GetPlayerNames() {
         if (!resourcesLoaded) {
@@ -105,5 +170,12 @@ public:
             return {};
         }
         return players;
+    }
+
+    std::map<std::string, Map*> GetMaps() {
+        if (!resourcesLoaded) {
+            return {};
+        }
+        return maps;
     }
 };
--- tests/gameloop/tests_game.cpp
+++ tests/gameloop/tests_game.cpp
@@ -1,4 +1,5 @@
 #include <gtest/gtest.h>
+
 #include "mockgameloop.h"
 
 TEST(GameResources, test_persistance_resources) {
@@ -7,6 +8,97 @@ TEST(GameResources, test_persistance_resources) {
     EXPECT_EQ(GameResources::GetInstance().IsResourcesLoaded(), true);
 }
 
-TEST(GameResources, test_persistance_resources2) {
+using namespace ActionsId;
+TEST(GameResources, test_movement_players) {
     EXPECT_EQ(GameResources::GetInstance().IsResourcesLoaded(), true);
+    GameResources::GetInstance().LoadResources();
+    // Obtencion de recursos
+    Queue<std::shared_ptr<GenericMsg>>& recv_queue = GameResources::GetInstance().GetRecvQueue();
+    // std::shared_ptr<PlayerObserver> player_obs =
+    // GameResources::GetInstance().GetPlayerObserver();
+
+    // Creacion de un nuevo juego
+    auto thread_loop = []() {
+        std::map<std::string, Map*> maps = GameResources::GetInstance().GetMaps();
+        SendQueuesMonitor<std::shared_ptr<GenericMsg>>& senders =
+                GameResources::GetInstance().GetSenders();
+        std::shared_ptr<std::set<uint>> ids = GameResources::GetInstance().GetIds();
+        EXPECT_NE(ids, nullptr);
+        Map* map = maps["test1"];
+        EXPECT_NE(map, nullptr);
+        Stage* current_stage = new Stage(*map, senders, ids);
+        EXPECT_NE(current_stage, nullptr);
+        std::shared_ptr<GameLoop> resources =
+                GameResources::GetInstance().UseResource(*map, current_stage);
+        EXPECT_NE(resources, nullptr);
+        resources->play_round(*current_stage, *map);
+    };
+    std::thread game_thread(thread_loop);
+
+
+    std::this_thread::sleep_for(std::chrono::milliseconds(20));  // Espera para que se cree el juego
+    // Verificacion de la creacion de los jugadores
+    std::map<std::string, Player*> players = GameResources::GetInstance().GetPlayers();
+    EXPECT_EQ(players.size(), 2);
+    EXPECT_EQ(players["player1"]->get_id(), 1);
+    EXPECT_EQ(players["player2"]->get_id(), 2);
+
+    // Verificacion que el player1 se mueva a la izquierda
+    Coordinate player1_pos = players["player1"]->get_position();
+    std::shared_ptr<GenericMsg> msg =
+            std::make_shared<StartActionMsg>(ActionId::MOVE_LEFT, "player1");
+    std::shared_ptr<GenericMsg> msg2 =
+            std::make_shared<StopActionMsg>(ActionId::MOVE_LEFT, "player1");
+    recv_queue.push(msg);
+    std::this_thread::sleep_for(
+            std::chrono::milliseconds(20));  // Espera para que se mueva el jugador
+    recv_queue.push(msg2);
+    std::this_thread::sleep_for(
+            std::chrono::milliseconds(20));  // Espera para que se mueva el jugador
+    Coordinate player1_new_pos = players["player1"]->get_position();
+    EXPECT_EQ(player1_pos.x - 1, player1_new_pos.x);
+    EXPECT_TRUE(player1_pos.y == player1_new_pos.y);
+
+    // Verificacion que el player2 se mueva a la derecha
+    Coordinate player2_pos = players["player2"]->get_position();
+    std::shared_ptr<GenericMsg> msg3 =
+            std::make_shared<StartActionMsg>(ActionId::MOVE_RIGHT, "player2");
+    std::shared_ptr<GenericMsg> msg4 =
+            std::make_shared<StopActionMsg>(ActionId::MOVE_RIGHT, "player2");
+    recv_queue.push(msg3);
+    std::this_thread::sleep_for(
+            std::chrono::milliseconds(20));  // Espera para que se mueva el jugador
+    recv_queue.push(msg4);
+    std::this_thread::sleep_for(
+            std::chrono::milliseconds(20));  // Espera para que se mueva el jugador
+    Coordinate player2_new_pos = players["player2"]->get_position();
+    EXPECT_EQ(player2_pos.x + 1, player2_new_pos.x);
+
+    // Verificacion que el player1 pueda saltar
+    player1_pos = players["player1"]->get_position();
+    std::shared_ptr<GenericMsg> msg5 = std::make_shared<StartActionMsg>(ActionId::JUMP, "player1");
+    std::shared_ptr<GenericMsg> msg6 = std::make_shared<StopActionMsg>(ActionId::JUMP, "player1");
+    recv_queue.push(msg5);
+    std::this_thread::sleep_for(
+            std::chrono::milliseconds(20));  // Espera para que se mueva el jugador
+    recv_queue.push(msg6);
+    std::this_thread::sleep_for(
+            std::chrono::milliseconds(20));  // Espera para que se mueva el jugador
+    player1_new_pos = players["player1"]->get_position();
+    EXPECT_EQ(player1_pos.x, player1_new_pos.x);
+    EXPECT_EQ(player1_pos.y - 2, player1_new_pos.y);
+
+    // Mato al jugador 1 para que termine la partida
+    EXPECT_FALSE(
+            players["player1"]->has_chest());  // Verifico que el jugador 1 no tenia nada equipado
+    EXPECT_FALSE(
+            players["player1"]->has_helmet());  // Verifico que el jugador 1 no tenia nada equipado
+    players["player1"]->die();
+    EXPECT_FALSE(players["player1"]->lives());  // Verifico que el jugador 1 haya muerto
+    EXPECT_FALSE(
+            players["player1"]->has_chest());  // Verifico que el jugador 1 no tenia nada equipado
+    EXPECT_FALSE(
+            players["player1"]->has_helmet());  // Verifico que el jugador 1 no tenia nada equipado
+    game_thread.join();
+    GameResources::GetInstance().freeNecesaryResources();
 }
